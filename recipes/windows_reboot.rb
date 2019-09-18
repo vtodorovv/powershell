4d8e76cae07e34cdc9d756a27d7ef4aa97c4327b
Veselin Todorov <44572504+vtodorovv@users.noreply.github.com>
2019-09-18 09:24:55 +0100

    Update windows_reboot.rb

--- recipes/windows_reboot.rb
+++ recipes/windows_reboot.rb
@@ -1,7 +1,7 @@
 # this recipe can be called from any recipe for windows reboot. Just specify default['powershell']['reboot_notifier']
 node.default['windows']['allow_pending_reboots'] = true
 node.default['windows']['allow_reboot_on_failure'] = true
-node.default['windows']['delay'] = 0
+node.default['windows']['delay'] = 1
 
 reboot 'powershell' do
   delay_mins node['windows']['delay']
